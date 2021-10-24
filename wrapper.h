6c8268e80b1f6cf74a474987a95344c5b0a3c54b
Sebastian Werhausen <swerhausen@gmail.com>
2021-10-24 09:59:29 +0200

    stream experiments

--- wrapper.h
+++ wrapper.h
@@ -7,6 +7,43 @@ namespace cvtsw
    template<typename T>
    concept std_string_type = std::same_as<T, std::string> || std::same_as<T, std::wstring>;
 
+   namespace detail {
+      template<char ending, int param_count>
+      struct super_type {
+         constexpr static char m_ending = ending;
+         int m_start_params[param_count];
+
+         template<typename ... Ts>
+         constexpr super_type(const Ts... params)
+            : m_start_params{ params... }
+         {}
+
+         template<cvtsw::std_string_type string_type>
+         operator string_type() const;
+
+         auto write(std::string& target) const;
+      };
+
+      using pos_type = super_type<'H', 2>;
+
+      //template<cvtsw::std_string_type string_type>
+      auto operator<<(std::string& target, const char* value) -> std::string&;
+      //template<cvtsw::std_string_type string_type>
+      auto operator<<(std::wstring& target, const wchar_t* value) -> std::wstring&;
+
+      template<cvtsw::std_string_type string_type, typename char_type = typename string_type::value_type>
+      auto operator<<(string_type& target, const char_type value) -> string_type&;
+      //auto operator<<(std::wstring& target, const wchar_t value) -> string_type&;
+      template<cvtsw::std_string_type string_type>
+      auto operator<<(string_type& target, const int value) -> string_type&;
+
+      template<typename stream_type, char ending, int param_count>
+      auto shift_impl(stream_type& os, const detail::super_type<ending, param_count>& value) -> stream_type&;
+   }
+
+   template<typename stream_type, char ending, int param_count>
+   auto operator<<(stream_type& os, const detail::super_type<ending, param_count>& value) -> stream_type&;
+
    // CHA; Cursor Horizontal Absolute
    template<cvtsw::std_string_type string_type>
    auto write_horizontal_pos(string_type& target, const int column) -> void;
@@ -20,10 +57,10 @@ namespace cvtsw
    [[nodiscard]] auto get_vertical_pos(const int line) -> string_type;
 
    // Zero-based, i.e. line=0, column=0 is top left
-   template<cvtsw::std_string_type string_type>
-   auto write_pos(string_type& target, const int line, const int column) -> void;
-   template<cvtsw::std_string_type string_type = std::string>
-   [[nodiscard]] auto get_pos(const int line, const int column) -> string_type;
+   //template<cvtsw::std_string_type string_type>
+   //auto write_pos(string_type& target, const int line, const int column) -> void;
+   //template<cvtsw::std_string_type string_type = std::string>
+   //[[nodiscard]] auto get_pos(const int line, const int column) -> string_type;
 
    // Foreground RGB color
    template<cvtsw::std_string_type string_type>
@@ -49,8 +86,30 @@ namespace cvtsw
    template<cvtsw::std_string_type string_type = std::string>
    [[nodiscard]] auto get_underline() -> string_type;
 
+   [[nodiscard]] auto position(const int line, const int column) -> detail::pos_type;
+
+   template<cvtsw::std_string_type string_type>
+   auto position(string_type& target, const int line, const int column) -> void;
+
    namespace detail
    {
+      template<typename T>
+      struct super_char_type {};
+      template<typename T>
+      struct super_char_type<std::basic_ostream<T>> {
+         using type = typename  std::basic_ostream<T>::char_type;
+      };
+      template<typename T>
+      struct super_char_type<std::basic_string<T>> {
+         using type = typename std::basic_string<T>::value_type;
+      };
+      template<typename T>
+      using super_char_type_t = typename super_char_type<T>::type;
+
+      //template<typename char_type, typename stream_type = std::basic_ostream<char_type>, char ending, int param_count>
+      //stream_type& operator<<(stream_type& os, const super_type<ending, param_count>& value);
+      
+
       template<cvtsw::std_string_type string_type>
       constexpr auto intro_str_v = "\x1b[";
       template<>
@@ -148,7 +207,7 @@ auto cvtsw::detail::get_generic(
 template<cvtsw::std_string_type string_type>
 auto cvtsw::write_horizontal_pos(string_type& target, const int column) -> void
 {
-   detail::write_generic(target, 'G', detail::to_xstring<string_type>(column + 1).c_str());
+   detail::write_generic(target, 'G', column + 1);
 }
 
 
@@ -164,7 +223,7 @@ auto cvtsw::get_horizontal_pos(const int column) -> string_type
 template<cvtsw::std_string_type string_type>
 auto cvtsw::write_vertical_pos(string_type& target, const int line) -> void
 {
-   detail::write_generic(target, 'd', detail::to_xstring<string_type>(line + 1).c_str());
+   detail::write_generic(target, 'd', line + 1);
 }
 
 
@@ -177,22 +236,22 @@ auto cvtsw::get_vertical_pos(const int line) -> string_type
 }
 
 
-template<cvtsw::std_string_type string_type>
-auto cvtsw::write_pos(string_type& target, const int line, const int column) -> void
-{
-   const int effective_line = line + 1;
-   const int effective_column = column + 1;
-   detail::write_generic(target, 'H', detail::to_xstring<string_type>(effective_line).c_str(), detail::to_xstring<string_type>(effective_column).c_str());
-}
-
-
-template<cvtsw::std_string_type string_type>
-auto cvtsw::get_pos(const int line, const int column) -> string_type
-{
-   string_type result;
-   write_pos(result, line, column);
-   return result;
-}
+//template<cvtsw::std_string_type string_type>
+//auto cvtsw::write_pos(string_type& target, const int line, const int column) -> void
+//{
+//   const int effective_line = line + 1;
+//   const int effective_column = column + 1;
+//   detail::write_generic(target, 'H', effective_line, effective_column);
+//}
+//
+//
+//template<cvtsw::std_string_type string_type>
+//auto cvtsw::get_pos(const int line, const int column) -> string_type
+//{
+//   string_type result;
+//   write_pos(result, line, column);
+//   return result;
+//}
 
 
 template<cvtsw::std_string_type string_type>
@@ -297,4 +356,113 @@ auto cvtsw::detail::to_xstring(const T value) -> string_type
       return std::to_string(value);
    else
       return std::to_wstring(value);
+}
+
+
+// maybe unite these two with a special concept
+//template<cvtsw::std_string_type string_type>
+auto cvtsw::detail::operator<<(
+   std::string& target,
+   const char* value
+) -> std::string&
+{
+   target += value;
+   return target;
+}
+
+
+//template<cvtsw::std_string_type string_type>
+auto cvtsw::detail::operator<<(
+   std::wstring& target,
+   const wchar_t* value
+   ) -> std::wstring&
+{
+   target += value;
+   return target;
+}
+
+
+template<cvtsw::std_string_type string_type, typename char_type>
+auto cvtsw::detail::operator<<(string_type& target, const char_type value)->string_type&
+{
+   // Necessary because otherwise he picks int overload
+   target += value;
+   return target;
+}
+
+
+template<cvtsw::std_string_type string_type>
+auto cvtsw::detail::operator<<(
+   string_type& target,
+   const int value
+) -> string_type&
+{
+   target += to_xstring<string_type>(value);
+   return target;
+}
+
+
+
+template<char ending, int param_count>
+template<cvtsw::std_string_type string_type>
+cvtsw::detail::super_type<ending, param_count>::operator string_type() const
+{
+   string_type result;
+   // TODO reserve
+   detail::shift_impl(result, *this);
+   return result;
+}
+
+
+
+template<typename stream_type, char ending, int param_count>
+auto cvtsw::detail::shift_impl(
+   stream_type& os,
+   const detail::super_type<ending, param_count>& value
+) -> stream_type&
+{
+   using char_type = super_char_type_t<stream_type>;
+   using string_type = std::basic_string<char_type>;
+
+   // This must be in detail because otherwise the string<< might leak into global namespace (with using decl)
+   os << detail::intro_str_v<string_type>;
+   for (int i = 0; i < std::size(value.m_start_params); ++i)
+   {
+      os << value.m_start_params[i];
+
+      // Semicolon between parameters
+      if (i != (std::size(value.m_start_params) - 1))
+         os << static_cast<char_type>(';');
+   }
+   os << value.m_ending;
+   return os;
+}
+
+
+template<typename stream_type, char ending, int param_count>
+stream_type& cvtsw::operator<<(stream_type& os, const detail::super_type<ending, param_count>& value)
+{
+   return detail::shift_impl(os, value);
+}
+
+template<char ending, int param_count>
+auto cvtsw::detail::super_type<ending, param_count>::write(std::string& target) const
+{
+   // TODO reserve
+   detail::shift_impl(target, *this);
+}
+
+
+auto cvtsw::position(const int line, const int column) -> detail::pos_type
+{
+   const int effective_line = line + 1;
+   const int effective_column = column + 1;
+   return detail::pos_type{ effective_line, effective_column };
+}
+
+
+template<cvtsw::std_string_type string_type>
+auto cvtsw::position(string_type& target, const int line, const int column) -> void
+{
+   return position(line, column).write(target);
 }
