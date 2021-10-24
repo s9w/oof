5e380377e53921055ed5b56ddd8df2d299168af2
Sebastian Werhausen <swerhausen@gmail.com>
2021-10-24 15:50:12 +0200

    refactoring

--- wrapper.h
+++ wrapper.h
@@ -9,59 +9,60 @@ namespace cvtsw
 
    namespace detail {
       template<char ending, int param_count>
-      struct super_type {
+      struct param_holder {
          int m_start_params[param_count];
 
          template<typename ... Ts>
-         constexpr explicit super_type(const Ts... params);
+         constexpr explicit param_holder(const Ts... params);
 
          template<cvtsw::std_string_type string_type>
          operator string_type() const;
       };
 
-      using pos_type = super_type<'H', 2>;
-      using hpos_type = super_type<'G', 1>;
-      using vpos_type = super_type<'d', 1>;
-      using underline_type = super_type<'m', 1>;
-      using reset_type = super_type<'m', 1>;
-      using color_type = super_type<'m', 5>;
-      using enum_color_type = super_type<'m', 1>;
+      using pos_params = param_holder<'H', 2>;
+      using hpos_params = param_holder<'G', 1>;
+      using vpos_params = param_holder<'d', 1>;
+      using underline_params = param_holder<'m', 1>;
+      using reset_params = param_holder<'m', 1>;
+      using rgb_color_params = param_holder<'m', 5>;
+      using enum_color_params = param_holder<'m', 1>;
    }
+   
 
-   template<typename stream_type, char ending, int param_count>
-   auto operator<<(stream_type& os, const detail::super_type<ending, param_count>& value) -> stream_type&;
-
-   [[nodiscard]] auto position(const int line, const int column) -> detail::pos_type;
+   [[nodiscard]] auto position(const int line, const int column) -> detail::pos_params;
    constexpr size_t   position_max = 3 + 2*3+1;
 
-   [[nodiscard]] auto vposition(const int line) -> detail::vpos_type;
+   [[nodiscard]] auto vposition(const int line) -> detail::vpos_params;
    constexpr size_t   vposition_max = 3 + 3;
 
-   [[nodiscard]] auto hposition(const int column) -> detail::hpos_type;
+   [[nodiscard]] auto hposition(const int column) -> detail::hpos_params;
    constexpr size_t   hposition_max = 3 + 3;
 
-   [[nodiscard]] auto fg_color(const int r, const int g, const int b) -> detail::color_type;
+   [[nodiscard]] auto fg_color(const int r, const int g, const int b) -> detail::rgb_color_params;
    constexpr size_t   fg_color_max = 3 + 16;
 
-   [[nodiscard]] auto bg_color(const int r, const int g, const int b) -> detail::color_type;
+   [[nodiscard]] auto bg_color(const int r, const int g, const int b) -> detail::rgb_color_params;
    constexpr size_t   bg_color_max = 3 + 16;
 
-   [[nodiscard]] auto underline(const bool new_value = true) -> detail::underline_type;
+   [[nodiscard]] auto underline(const bool new_value = true) -> detail::underline_params;
    constexpr size_t   underline_max = 3 + 2;
 
-   [[nodiscard]] auto reset_formatting() -> detail::reset_type;
+   [[nodiscard]] auto reset_formatting() -> detail::reset_params;
 
    enum class color {black = 30, red, green, yellow, blue, magenta, cyan, white, reset = 39 };
-   [[nodiscard]] auto fg_color(const color color) -> detail::enum_color_type;
+   [[nodiscard]] auto fg_color(const color color) -> detail::enum_color_params;
    
 
    namespace detail
    {
+      template<typename stream_type, char ending, int param_count>
+      auto operator<<(stream_type& os, const detail::param_holder<ending, param_count>& value)->stream_type&;
+
       template<cvtsw::std_string_type string_type>
       auto reserve_size(string_type& target, const size_t needed_size) -> void;
 
       template<cvtsw::std_string_type string_type, char ending, int param_count>
-      auto write_to_string(string_type& target, const detail::super_type<ending, param_count>& value) -> void;
+      auto write_to_string(string_type& target, const detail::param_holder<ending, param_count>& value) -> void;
 
       template<int n>
       [[nodiscard]] constexpr auto get_reserve_size(const int(& ary)[n]) -> size_t;
@@ -106,7 +107,7 @@ auto cvtsw::detail::to_xstring(const T value) -> string_type
 
 template<char ending, int param_count>
 template<cvtsw::std_string_type string_type>
-cvtsw::detail::super_type<ending, param_count>::operator string_type() const
+cvtsw::detail::param_holder<ending, param_count>::operator string_type() const
 {
    string_type result;
    reserve_size(result, get_reserve_size(m_start_params));
@@ -116,7 +117,10 @@ cvtsw::detail::super_type<ending, param_count>::operator string_type() const
 
 
 template<cvtsw::std_string_type string_type, char ending, int param_count>
-auto cvtsw::detail::write_to_string(string_type& target, const detail::super_type<ending, param_count>& value) -> void
+auto cvtsw::detail::write_to_string(
+   string_type& target,
+   const detail::param_holder<ending, param_count>& value
+) -> void
 {
    using char_type = typename string_type::value_type;
    if constexpr (std::same_as<string_type, std::string>)
@@ -150,11 +154,13 @@ auto cvtsw::detail::reserve_size(
 
 
 template<typename stream_type, char ending, int param_count>
-auto cvtsw::operator<<(
+auto cvtsw::detail::operator<<(
    stream_type& os,
-   const detail::super_type<ending, param_count>& value
+   const detail::param_holder<ending, param_count>& value
 ) -> stream_type&
 {
+   // Stream operation is _massively_ slow, therefore write to string and stream that in one go
+
    using char_type = typename stream_type::char_type;
    using string_type = std::basic_string<char_type>;
 
@@ -168,7 +174,7 @@ auto cvtsw::operator<<(
 
 template<char ending, int param_count>
 template<typename ... Ts>
-constexpr cvtsw::detail::super_type<ending, param_count>::super_type(
+constexpr cvtsw::detail::param_holder<ending, param_count>::param_holder(
    const Ts... params
 )
    : m_start_params{ params... }
@@ -181,49 +187,41 @@ constexpr cvtsw::detail::super_type<ending, param_count>::super_type(
 
 #ifdef CM_IMPL
 
-auto cvtsw::position(const int line, const int column) -> detail::pos_type
-{
+auto cvtsw::position(const int line, const int column) -> detail::pos_params{
    const int effective_line = line + 1;
    const int effective_column = column + 1;
-   return detail::pos_type{ effective_line, effective_column };
+   return detail::pos_params{ effective_line, effective_column };
 }
 
-auto cvtsw::vposition(const int line)->detail::vpos_type
-{
+auto cvtsw::vposition(const int line)-> detail::vpos_params{
    const int effective_line = line + 1;
-   return detail::vpos_type{ effective_line };
+   return detail::vpos_params{ effective_line };
 }
 
-auto cvtsw::hposition(const int column) -> detail::hpos_type
-{
+auto cvtsw::hposition(const int column) -> detail::hpos_params{
    const int effective_column = column + 1;
-   return detail::hpos_type{ effective_column };
+   return detail::hpos_params{ effective_column };
 }
 
-auto cvtsw::fg_color(const int r, const int g, const int b) -> detail::color_type
-{
-   return detail::color_type{ 38, 2, r, g, b };
+auto cvtsw::fg_color(const int r, const int g, const int b) -> detail::rgb_color_params{
+   return detail::rgb_color_params{ 38, 2, r, g, b };
 }
 
-auto cvtsw::fg_color(const color color) -> detail::enum_color_type
-{
-   return detail::enum_color_type{ static_cast<int>(color) };
+auto cvtsw::fg_color(const color color) -> detail::enum_color_params{
+   return detail::enum_color_params{ static_cast<int>(color) };
 }
 
-auto cvtsw::bg_color(const int r, const int g, const int b) -> detail::color_type
-{
-   return detail::color_type{ 48, 2, r, g, b };
+auto cvtsw::bg_color(const int r, const int g, const int b) -> detail::rgb_color_params{
+   return detail::rgb_color_params{ 48, 2, r, g, b };
 }
 
-auto cvtsw::underline(const bool new_value) -> detail::underline_type
-{
+auto cvtsw::underline(const bool new_value) -> detail::underline_params{
    const int code = new_value == true ? 4 : 24;
-   return detail::underline_type{ code };
+   return detail::underline_params{ code };
 }
 
-auto cvtsw::reset_formatting() -> detail::reset_type
-{
-   return detail::reset_type{ 0 };
+auto cvtsw::reset_formatting() -> detail::reset_params{
+   return detail::reset_params{ 0 };
 }
 
 #endif
