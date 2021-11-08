2a1194bdef18fcc42178a51174f17d112a90a574
Sebastian Werhausen <swerhausen@gmail.com>
2021-11-08 17:06:37 +0100

    demo code improvements

--- cpp_vt_wrapper/cursor_trail_demo.cpp
+++ cpp_vt_wrapper/cursor_trail_demo.cpp
@@ -8,6 +8,7 @@ using namespace cvtsw;
 
 #include <s9w/s9w_rng.h>
 #include <s9w/s9w_geom_types.h>
+#include <s9w/s9w_colors.h>
 
 namespace {
 
@@ -42,11 +43,12 @@ namespace {
       return scree_cell_dimensions * font_pixel_size;
    }
 
-   // Source: https://devblogs.microsoft.com/oldnewthing/20131017-00/?p=2903
+   
    auto get_window_rect() -> RECT{
       RECT window_rect{};
       GetWindowRect(GetConsoleWindow(), &window_rect);
 
+      // Corrections, see https://devblogs.microsoft.com/oldnewthing/20131017-00/?p=2903
       RECT corrections{};
       const DWORD dwStyle = static_cast<DWORD>(GetWindowLong(GetConsoleWindow(), GWL_STYLE));
       const DWORD dwExStyle = static_cast<DWORD>(GetWindowLong(GetConsoleWindow(), GWL_EXSTYLE));
@@ -70,40 +72,41 @@ namespace {
       };
    }
 
-   // Ideal if: 0.5 -> 0; 0 -> 0.5; -0.5 -> 1.0
+
+   // My go-to approximation of mapping SDF values to intensity
    auto get_sdf_intensity(const double sdf) -> double {
       return std::clamp(-sdf + 0.5, 0.0, 1.0);
    }
 
-   auto get_multiplied_color(
-      const color& col,
-      const double factor
-   ) -> color {
-      return color{
-         get_int<uint8_t>(std::clamp(col.red * factor, 0.0, 255.0)),
-         get_int<uint8_t>(std::clamp(col.green * factor, 0.0, 255.0)),
-         get_int<uint8_t>(std::clamp(col.blue * factor, 0.0, 255.0))
-      };
-   }
-
    auto get_max_color(
-      const color& a,
-      const color& b
-   ) -> color {
-      return color{
-         std::max(a.red, b.red),
-         std::max(a.green, b.green),
-         std::max(a.blue, b.blue)
+      const s9w::srgb_u& a,
+      const s9w::srgb_u& b
+   ) -> s9w::srgb_u
+   {
+      return s9w::srgb_u{
+         std::max(a.r, b.r),
+         std::max(a.g, b.g),
+         std::max(a.b, b.b)
       };
    }
 
-   auto get_faded_color(const color& col, const int fade_amount) -> color{
-      return color{
-         static_cast<uint8_t>(std::clamp(col.red - fade_amount, 0, 255)),
-         static_cast<uint8_t>(std::clamp(col.green - fade_amount, 0, 255)),
-         static_cast<uint8_t>(std::clamp(col.blue - fade_amount, 0, 255))
-      };
+   // This might be overkill, but this correctly dims the lightness in an appropriate (non-sRGB) colorspace
+   auto get_faded_color(const color& col, const double fade_amount) -> color{
+      if (col == color{})
+         return color{};
+      s9w::oklab_d okl = s9w::convert_color<s9w::oklab_d>(std::bit_cast<s9w::srgb_u>(col));
+      okl.L = std::clamp(okl.L - fade_amount, 0.0, 1.0);
+      return std::bit_cast<color>(s9w::convert_color<s9w::srgb_u>(okl));
    }
+
+   auto get_draw_color(const double time) -> s9w::srgb_u {
+      const double hue = std::fmod(50.0 * time, 360.0);
+      constexpr double saturation = 100.0;
+      constexpr double lightness = 50.0;
+      const s9w::hsluv_d hsluv_color{ hue , saturation, lightness };
+      return s9w::convert_color<s9w::srgb_u>(hsluv_color);
+   }
+
 } // namespace {}
 
 auto cursor_trail_demo() -> void
@@ -115,19 +118,18 @@ auto cursor_trail_demo() -> void
    const RECT window_rect = get_window_rect();
    pixel_screen canvas(canvas_dimensions[0], canvas_dimensions[1], 0, 0, color{});
    timer timer;
-   color draw_color{ 255, 0, 0 };
    double fade_amount = 0.0;
 
    while (true) {
       // Fading
-      fade_amount += 200.0 * timer.get_dt();
-      if (fade_amount >= 1.0) {
-         const int effective_amount = static_cast<int>(fade_amount);
-         fade_amount -= effective_amount;
-
+      fade_amount += timer.get_dt();
+      if (fade_amount >= 0.01) {
          for (color& col : canvas)
-            col = get_faded_color(col, effective_amount);
+            col = get_faded_color(col, fade_amount);
+         fade_amount = 0.0;
       }
+
+      const s9w::srgb_u draw_color = get_draw_color(timer.get_seconds_since_start());
 
       // Drawing of the circle with SDFs
       const s9w::dvec2 cursor_cell_pos = s9w::dvec2{ get_cursor_pixel_pos(window_rect) } / s9w::dvec2{ halfline_font_pixel_size };
@@ -138,18 +140,20 @@ auto cursor_trail_demo() -> void
             constexpr double circle_radius = 5.0;
             const double circle_sdf = s9w::get_length(cell_pos - cursor_cell_pos) - circle_radius;
 
-            const color effective_brush_color = get_multiplied_color(draw_color, get_sdf_intensity(circle_sdf));
-            canvas.get_color(column, halfline) = get_max_color(canvas.get_color(column, halfline), effective_brush_color);
+
+            const s9w::srgb_u effective_brush_color = get_sdf_intensity(circle_sdf) * draw_color;
+            const s9w::srgb_u canvas_color = std::bit_cast<s9w::srgb_u>(canvas.get_color(column, halfline));
+            const s9w::srgb_u result_color = get_max_color(canvas_color, effective_brush_color);
+            canvas.get_color(column, halfline) = std::bit_cast<color>(result_color);
          }
       }
 
       // Timing things; FPS; change of color
       timer.mark_frame();
       fast_print(canvas.get_string(color{ 0, 0, 0 }));
+
       const auto fps = timer.get_fps();
-      if (fps.has_value()) {
+      if (fps.has_value())
          set_window_title("FPS: " + std::to_string(*fps));
-         draw_color = get_random_color(rng);
-      }
    }
 }
