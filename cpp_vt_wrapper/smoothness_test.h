c5f85f371d6ac874ea048ee35fa034a7422df72a
Sebastian Werhausen <swerhausen@gmail.com>
2021-11-15 17:40:51 +0100

    test cleanup

--- cpp_vt_wrapper/smoothness_test.h
+++
@@ -1,3 +0,0 @@
-#pragma once
-
-auto smoothness_test() -> void;
--- tests/tests.cpp
+++ tests/tests.cpp
@@ -3,33 +3,28 @@
 #define DOCTEST_CONFIG_IMPLEMENT
 #include "doctest.h"
 
-#define CM_IMPL
+#define OOF_IMPL
 #include "../oof.h"
 
-#include "perf_tests.h"
-
 namespace {
-
-}
-
-auto run_doctest() -> std::optional<int> {
-   doctest::Context context;
-   const int res = context.run();
-   if (context.shouldExit())
-      return res;
-   return std::nullopt;
+   auto run_doctest() -> std::optional<int> {
+      doctest::Context context;
+      const int res = context.run();
+      if (context.shouldExit())
+         return res;
+      return std::nullopt;
+   }
 }
 
 
 auto main() -> int
 {
-   std::cout << "\x1b[?25l"; // no cursor
+   std::cout << oof::cursor_visibility(false) << oof::reset_formatting() << oof::clear_screen_sequence();
 
    const std::optional<int> doctest_result = run_doctest();
    if (doctest_result.has_value())
       return doctest_result.value();
 
-   //run_perf_tests();
    return 0;
 }
 
